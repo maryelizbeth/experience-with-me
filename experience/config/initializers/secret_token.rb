f89ffc8d808629c105d9f45e9e0f52f5b37dafcd
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-21 14:56:22 -0400

    application framework for front-end:

---
+++ experience/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Experience::Application.config.secret_token = 'bb01a973499685f7558a377067bb760216f6e8b8f1ed710cd8356566eafeadd51e4cdfc60e42c0bd6095d8c683c4645f48f7dc9470437cac1e2a2a5d1c47c24b'
