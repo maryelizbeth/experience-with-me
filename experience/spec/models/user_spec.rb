5b6e8abd7c7cb73e440f043e521fc20263ee897c
Mary Cutrali <mary.cutrali@gmail.com>
2012-06-25 00:06:08 -0400

    begin integration testing

--- experience/spec/models/user_spec.rb
+++ experience/spec/models/user_spec.rb
@@ -1,3 +1,12 @@
 require 'spec_helper'
-  
+
+describe User do
+  context "Signing Up with GMail" do 
+    
+    describe "GET /auth/google" do
+      it "directs the user to google" do 
+        page.should have_content("A third party service is requesting permission to access your Google Account.")
+      end 
+    end 
+  end
 end
